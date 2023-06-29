e6fe2108849cc74eb9983ce42d2436c723298299
mathew puig <puigster956@gmail.com>
2023-06-29 00:30:49 -0700

    adding home page view

---
+++ views/home.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('./default')
+
+function home() {
+    return (
+        <def>
+            <main>
+                <h1>HOME</h1>
+            </main>
+        </def>
+    )
+}
+
+module.exports = home
